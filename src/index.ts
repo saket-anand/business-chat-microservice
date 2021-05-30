4deabdb8ffbe2ce23792a92d2a1b2436725541b5
Saket Anand <just1saket@gmail.com>
2021-05-30 20:37:47 +0530

    dockerized the project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-function displayName(fname: string, lname: string): string {
+require('dotenv').config();
+
+function displayName(fname: string, lname?: string): string {
     console.log(fname + lname);
     return `${fname} ${lname}`;
 }
+displayName('Saket Anand ', process.env.DB_HOST);
